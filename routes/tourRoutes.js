7139b7a82c419b6530c2e9a004e6d73970a25a3d
George ARION <georgearion@gmail.com>
2020-09-08 16:31:11 +0700

    chore(core api - middleware): chaining multiple middleware functions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create checkBody middleware
+// Check if body contains the name and the property
+// if not, send back 400 (bad request)
+// add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
