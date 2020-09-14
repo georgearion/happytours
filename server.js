53ad7e9c826702b6b463a4f648f230071586b8bb
George ARION <georgearion@gmail.com>
2020-09-14 11:24:18 +0700

    chore(error handling): catching uncaught exceptions #54

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// sync -Uncaught Exceptions
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -23,9 +30,10 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// async -Unhandled Promise Rejections
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
