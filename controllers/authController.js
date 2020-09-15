ba9265b67d1e6d8888ee3aa5d258229cc3fa62b9
George ARION <georgearion@gmail.com>
2020-09-15 19:52:17 +0700

    chore(authorization): password reset token #62

--- controllers/authController.js
+++ controllers/authController.js
@@ -117,3 +117,19 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email address
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with that email address', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3) Send token to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
