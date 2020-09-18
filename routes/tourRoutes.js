f36943126ccf1ad4b72bbe3ad0dae2ff924d8f79
George ARION <georgearion@gmail.com>
2020-09-18 16:19:04 +0700

    chore(core api - nested routes): nested routes with express #82

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 //router.param('id', tourController.checkID);
+
+// MOUNTING ROUTER
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -34,14 +37,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
