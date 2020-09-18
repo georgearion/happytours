1cf7c576da9be9a2614e0bee109393d599e25395
George ARION <georgearion@gmail.com>
2020-09-18 11:24:31 +0700

    chore(core api - reviews): modelling reviews - parent referencing #77

---
+++ models/reviewModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty!']
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
+    },
+    createdAt: {
+      Date,
+      default: Date.now()
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.']
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
+
+//review / rating / createdAt / ref to tour / ref to User
