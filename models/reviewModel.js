60cbb749b5336db02c10fe8f284471779fa11b8e
George ARION <georgearion@gmail.com>
2020-09-18 13:44:51 +0700

    chore(core api - reviews): populating reviews #79

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,21 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// QUERY MIDDLEWARE:
+// runs before the .save() and .create()
+// doesn't run before .insertMany()
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
