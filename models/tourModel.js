277819b8c7f58b5d582948141047f944b0d423c1
George ARION <georgearion@gmail.com>
2020-09-21 22:44:30 +0700

    feat(core api - geospatial data): aggregation - calculating distances #94

--- models/tourModel.js
+++ models/tourModel.js
@@ -193,11 +193,11 @@ tourSchema.post(/^find/, function(docs, next) {
 });
 
 // AGGREGATION MIDDLEWARES
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  //console.log(this.pipeline());
-  next();
-});
+// tourSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//   //console.log(this.pipeline());
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -29,6 +29,9 @@ router
   .route('/tours-within/:distance/center/:latlng/unit/:unit')
   .get(tourController.getToursWithin);
 
+// /distances/-40,45/unit/km
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
