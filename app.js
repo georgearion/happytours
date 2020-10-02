64f7cb72dcff1789dd89ea644f529d46abaad50a
George ARION <georgearion@gmail.com>
2020-10-02 13:34:36 +0700

    chore(ssr with pug): first steps with pug

--- app.js
+++ app.js
@@ -74,7 +74,10 @@ app.use((req, res, next) => {
 
 // 2) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'Tour Title',
+    user: 'George'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
