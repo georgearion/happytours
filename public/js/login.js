d5761cb0105662f2f1cc7d2c0443b0f1e8a75a58
George ARION <georgearion@gmail.com>
2020-10-09 19:25:38 +0700

    chore(frontend - users): logging out users #110

--- public/js/login.js
+++ public/js/login.js
@@ -25,3 +25,21 @@ export const login = async (email, password) => {
     showAlert('error', err.response.data.message);
   }
 };
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout'
+    });
+
+    if ((res.data.status = 'success')) {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.reload(true);
+      }, 1000);
+    }
+  } catch (err) {
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
