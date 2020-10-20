c705f8cb1592115fd2d596aeebd17453361b08ea
George ARION <georgearion@gmail.com>
2020-10-20 11:47:35 +0700

    feat(advanced features - file uploads): image uploads using multer #116

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const multer = require('multer');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
+
+const upload = multer({ dest: 'public/img/users'});
 
 const router = express.Router();
 
@@ -15,7 +18,7 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.use(authController.protect);
 
 router.get('/me', userController.getMe, userController.getUser);
-router.patch('/updateMe', userController.updateMe);
+router.patch('/updateMe', upload.single('photo'), userController.updateMe);
 router.patch('/updateMyPassword', authController.updatePassword);
 router.delete('/deleteMe', userController.deleteMe);
 
