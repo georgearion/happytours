d2450428b47dc3cbcbd639516def0253962172bd
George ARION <georgearion@gmail.com>
2020-10-22 11:55:30 +0700

    feat(advanced features - bookings): modelling the bookings #129

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9080,7 +9080,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52421" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50817" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
