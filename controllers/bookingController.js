f73910fd52ded867cb71d8c8aabb6348e9eb5bfe
George ARION <georgearion@gmail.com>
2020-10-22 15:01:16 +0700

    feat(advanced features - bookings): creating new bookings on checkout success #130

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,17 +1,20 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const Tour = require('./../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+const catchAsync = require('../utils/catchAsync');
 // const factory = require('./handlerFactory');
 // const AppError = require('./../utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour session
+  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -32,4 +35,17 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  // 1) Check if Booking data is available on query string
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) return next();
+
+  // 2) Create a Booking entry
+  await Booking.create({ tour, user, price });
+
+  // 3) Redirect application without query string
+  res.redirect(req.originalUrl.split('?')[0]);
 });
