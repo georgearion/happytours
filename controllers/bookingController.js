660ae6e80993f62ad48677d95e7f2c21d731e83d
George ARION <georgearion@gmail.com>
2020-11-12 11:15:10 +0700

    fix(advanced features - payments): checkout payments with stripe webhooks #158

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -24,7 +24,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `https://happytours.herokuapp.com/img/tours/${tour.imageCover}`
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1
